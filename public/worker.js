df792abdfca7861eb41dd0b3052d7660da774ea1
Sabina Krsmanovic <sabinakrsmanovic@Sabinas-MacBook-Air.local>
2026-04-17 14:21:50 +0200

    fix: completely disable worker

--- public/worker.js
+++ public/worker.js
@@ -1,5 +1,8 @@
-// Worker image loading is disabled. CloudFront does not return CORS headers,
-// so fetch() cannot be used here. All image loading is handled on the main thread.
 self.onmessage = (e) => {
-  self.postMessage({ message: 'error', error: 'Worker image loading disabled', index: e.data.index, url: e.data.url });
-}
+  self.postMessage({
+    message: 'error',
+    error: 'Worker disabled in production',
+    index: e.data?.index,
+    url: e.data?.url
+  });
+};
